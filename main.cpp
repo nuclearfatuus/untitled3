bc4993e1b270b48374df7b14cbbbcf5e84003fb2
nuclearfatuus <nuclearfatuus@gmail.com>
2018-11-16 15:49:25 +0300

    trying to do the first task

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 
+class task {
+    int a;
+    char c;
+    int *x;
+    int y;
+
+public:
+    int get_a() {
+        return a;
+    };
+
+    char get_c() {
+        return c;
+    };
+
+    int set_a(int a){
+        task::a = a;
+    };
+
+    char set_c(char c){
+        task::c = c;
+    };
+
+    int* get_aa(){
+        x = &a;
+        return &a;
+    };
+
+    int set_aa(int& a){
+        task::y = a;
+    };
+
+
+};
+
 int main() {
-    char c = 'k';
-    int a = 3;
-    std::cout << c/a << std::endl;
-    std::cout << c*a << std::endl;
-    std::cout << c-a << std::endl;
-    std::cout << c+a << std::endl;
-    std::cout << c/float(a) << std::endl;
-    c = c/a;
-    std::cout << c << std::endl;
-    c = c*a*a;
-    std::cout << c << std::endl;
-    c = (c/(a^2))+a;
-    std::cout << c << std::endl;
-    c = c-2*a;
-    std::cout << c << std::endl;
-    return 0;
+
+    task a;
+    a.get_a();
+    a.set_a(25);
+    a.get_aa();
+    int x = 2;
+    a.set_aa(x);
+
+    task c;
+    c.get_c();
+    c.set_c(1);
+
+return 0;
 }
